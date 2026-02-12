ac029d22fef252251507bbe3e442c8dae7e7ae3a
Abdul Mannan <245386525+abdulmannan-556@users.noreply.github.com>
2026-02-12 15:39:14 +0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
+import logo from "../assets/logo.png";
 
+const Header = () => {
+  return (
+    <header className="bg-white shadow-md">
+      <div className="container flex items-center justify-between py-4">
+        {/* Logo */}
+        <Link to="/">
+          <img src={logo} alt="Fashionista PK Logo" className="h-10" />
+        </Link>
+
+        {/* Navigation */}
+        <Navbar />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
