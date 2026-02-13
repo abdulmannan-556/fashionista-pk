71563c5acda36e6d8a6b9a71023d48715c2c85b9
Abdul Mannan <245386525+abdulmannan-556@users.noreply.github.com>
2026-02-13 12:03:40 +0000

    commit changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles/index.css"; // TailwindCSS import
+import App from "./App.jsx";
+import "./index.css";
+import { CartProvider } from "./context/CartContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <CartProvider>
+      <App />
+    </CartProvider>
   </React.StrictMode>
 );
