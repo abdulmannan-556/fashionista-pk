0292aa5004db9409cc141d62bdfba9439b82cf9e
Abdul Mannan <245386525+abdulmannan-556@users.noreply.github.com>
2026-02-13 11:59:27 +0000

    commit changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx}"
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#4f46e5", // Indigo for buttons and accents
-        secondary: "#6b7280", // Gray for text
-      },
-      fontFamily: {
-        sans: ["Inter", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
