0773527cc8eee5cee320c27720c89b979665efe3
Abdul Mannan <245386525+abdulmannan-556@users.noreply.github.com>
2026-02-21 15:35:51 +0500

    Added login and register routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home.jsx";
 import Shop from "./pages/Shop.jsx";
 import Product from "./pages/Product.jsx";
 import Cart from "./pages/Cart.jsx";
 import About from "./pages/About.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 
@@ -12,13 +16,20 @@ const App = () => {
   return (
     <Router>
       <Header />
+
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/product/:id" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/about" element={<About />} />
+
+        {/* Auth Routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
+
       <Footer />
     </Router>
   );
